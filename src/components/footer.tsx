e9d200611a67e6150c2e954cf67d8a9c6250d703
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-02-08 17:31:53 +0900

    🩹 chore

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,8 @@ import { Box, Link } from '@chakra-ui/react';
 export const Footer = () => {
   return (
     <Box align='center' opacity={0.4} fontSize='sm'>
-      Thanks a lot, <Link href='https://www.craftz.dog/'>@craftdog</Link>
+      &copy; {new Date().getFullYear()} Pandashark. All Rights Reserved. Thanks a lot,{' '}
+      <Link href='https://www.craftz.dog/'>@craftdog</Link>
     </Box>
   );
 };
